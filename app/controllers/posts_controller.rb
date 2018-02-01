c460de3ec7a70030e71dae92d54a47c894220a83
Rory0172 <rory0172@live.nl>
2018-02-01 12:46:09 +0100

    create add new post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,17 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
+    @category = Category.all
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, :notice => "Your post has been saved"
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -24,6 +30,10 @@ class PostsController < ApplicationController
 
 
   def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
   end
 
 end
