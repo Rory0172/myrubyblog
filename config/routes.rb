aae3efbca091aaae06b7b48d9d77cf2bc0772df9
Rory0172 <rory0172@live.nl>
2018-02-02 11:59:10 +0100

    add some content to home page, made main nav, add about&contact&resources pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/resources'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'home/index'
-    resources :posts
-    resources :categories
   root :to => 'home#index'
+  resources :posts
+  resources :categories
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  get '/resources', to: 'pages#resources'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
