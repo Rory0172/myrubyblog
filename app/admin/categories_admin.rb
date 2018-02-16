783377fef232612adbee1da569c1b896743a450e
Rory0172 <rory0172@live.nl>
2018-02-16 16:17:08 +0100

    removed useless inputs edit category admin page

--- app/admin/categories_admin.rb
+++ app/admin/categories_admin.rb
@@ -12,14 +12,9 @@ Trestle.resource(:categories) do
 
   # Customize the form fields shown on the new/edit views.
   #
-  # form do |category|
-  #   text_field :name
-  #
-  #   row do
-  #     col(xs: 6) { datetime_field :updated_at }
-  #     col(xs: 6) { datetime_field :created_at }
-  #   end
-  # end
+  form do |category|
+    text_field :name
+  end
 
   # By default, all parameters passed to the update and create actions will be
   # permitted. If you do not have full trust in your users, you should explicitly
